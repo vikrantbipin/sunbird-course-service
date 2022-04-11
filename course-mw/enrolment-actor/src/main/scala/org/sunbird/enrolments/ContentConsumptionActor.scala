3436c6c3f33984f0c80139d18276ad766e3a7ee0
nivetha <nivetha.mariappan@tarento.com>
2022-04-11 15:47:43 +0530

    Content progress data type fix

--- course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/ContentConsumptionActor.scala
+++ course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/ContentConsumptionActor.scala
@@ -379,7 +379,8 @@ class ContentConsumptionActor @Inject() extends BaseEnrolmentActor {
                 m.put(JsonKey.COLLECTION_ID, m.getOrDefault(JsonKey.COURSE_ID, ""))
                 jsonFields.foreach(field =>
                     if(fields.contains(field))
-                        m.put(field, m.getOrDefault(field,""))
+                        m.put(field, mapper.readTree(m.get(field).asInstanceOf[String]))
+//                        m.put(field, m.getOrDefault(field,""))
                 )
                 val formattedMap = JsonUtil.convertWithDateFormat(m, classOf[util.Map[String, Object]], dateFormatter)
                 if (fields.contains(JsonKey.ASSESSMENT_SCORE))
