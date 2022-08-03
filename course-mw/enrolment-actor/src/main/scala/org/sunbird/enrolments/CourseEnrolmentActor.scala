150938a30c70b354877bf61f09d04822e3a98db5
wilkysingh-tarento <wilky.singh@tarento.com>
2022-08-03 13:20:09 +0530

    Date format modified for batchProgram email invite template

--- course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/CourseEnrolmentActor.scala
+++ course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/CourseEnrolmentActor.scala
@@ -79,6 +79,12 @@ class CourseEnrolmentActor @Inject()(@Named("course-batch-notification-actor") c
         val userId: String = request.get(JsonKey.USER_ID).asInstanceOf[String]
         val batchId: String = request.get(JsonKey.BATCH_ID).asInstanceOf[String]
         val batchData: CourseBatch = courseBatchDao.readById( courseId, batchId, request.getRequestContext)
+        import java.text.SimpleDateFormat
+        val format1 = new SimpleDateFormat("yyyy-MM-dd")
+        val startDateInNewFormat = format1.format(batchData.getStartDate);
+        batchData.setStartDate(format1.parse(startDateInNewFormat))
+        val endDateInNewFormat = format1.format(batchData.getEndDate);
+        batchData.setEndDate(format1.parse(endDateInNewFormat))
         val enrolmentData: UserCourses = userCoursesDao.read(request.getRequestContext, userId, courseId, batchId)
         validateEnrolment(batchData, enrolmentData, true)
         val data: java.util.Map[String, AnyRef] = createUserEnrolmentMap(userId, courseId, batchId, enrolmentData, request.getContext.getOrDefault(JsonKey.REQUEST_ID, "").asInstanceOf[String])
