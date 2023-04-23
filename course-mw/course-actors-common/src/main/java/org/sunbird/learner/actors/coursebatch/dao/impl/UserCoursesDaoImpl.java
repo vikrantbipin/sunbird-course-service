5c23fa6bf11f1760130888db35c04cd2df7226db
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2023-04-23 18:34:36 +0530

    4.8.0 lookup enhancement v2 (#37)
    * Added pagination capability for get participants API

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
@@ -1,8 +1,10 @@
 package org.sunbird.learner.actors.coursebatch.dao.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
+
 import java.util.*;
-import java.util.stream.Collectors;
 import org.apache.commons.collections.CollectionUtils;
 import org.sunbird.cassandra.CassandraOperation;
 import org.sunbird.common.Constants;
@@ -13,9 +15,10 @@ import org.sunbird.helper.ServiceFactory;
 import org.sunbird.learner.actors.coursebatch.dao.UserCoursesDao;
 import org.sunbird.learner.util.Util;
 import org.sunbird.models.user.courses.UserCourses;
+import org.sunbird.common.models.util.LoggerUtil;
 
 public class UserCoursesDaoImpl implements UserCoursesDao {
-
+  protected LoggerUtil logger = new LoggerUtil(this.getClass()); 
   private CassandraOperation cassandraOperation = ServiceFactory.getInstance();
   private ObjectMapper mapper = new ObjectMapper();
   static UserCoursesDao userCoursesDao;
@@ -46,6 +49,7 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
     try {
       return mapper.convertValue((Map<String, Object>) userCoursesList.get(0), UserCourses.class);
     } catch (Exception e) {
+      logger.error(requestContext, "Failed to read user enrollment table. Exception: ", e);
     }
     return null;
   }
@@ -112,6 +116,7 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
     try {
       return mapper.convertValue((Map<String, Object>) userCoursesList.get(0), UserCourses.class);
     } catch (Exception e) {
+      logger.error(requestContext, "Failed to read user enrollments table. Exception: ", e);
     }
     return null;
   }
@@ -121,7 +126,7 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
     Map<String, Object> queryMap = new HashMap<>();
     queryMap.put(JsonKey.BATCH_ID, batchId);
     Response response =
-            cassandraOperation.getRecordByIdentifier(requestContext,KEYSPACE_NAME, ENROLMENT_BATCH_LOOKUP, queryMap, null, Constants.DEFAULT_LIMIT);
+            cassandraOperation.getRecordByIdentifier(requestContext,KEYSPACE_NAME, ENROLMENT_BATCH_LOOKUP, queryMap, null);
         /*cassandraOperation.getRecords(
                 requestContext, KEYSPACE_NAME, USER_ENROLMENTS, queryMap, Arrays.asList(JsonKey.USER_ID, JsonKey.ACTIVE));*/
     List<Map<String, Object>> userCoursesList =
@@ -153,5 +158,77 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
     } else {
       return userCoursesList;
     }
+  }
+
+  @Override
+  public Map<String, Object> getBatchParticipantsByPage(RequestContext requestContext, Map<String, Object> request) {
+    logger.info(requestContext, "UserCourseDao:: getBatchParticipantsByPage:: Received request:: " + request);
+    Map<String, Object> queryMap = new HashMap<>();
+    queryMap.put(JsonKey.BATCH_ID, (String) request.get(JsonKey.BATCH_ID));
+    Map<String, Object> result = new HashMap<String, Object>();
+    List<String> userList = new ArrayList<String>();
+    Boolean active = (Boolean) request.get(JsonKey.ACTIVE);
+    if (null == active) {
+      active = true;
+    }
+    Integer limit = (Integer) request.get(JsonKey.LIMIT);
+    if (limit == null) {
+      limit = Constants.DEFAULT_LIMIT;
+    }
+    Integer currentOffSetFromRequest = (Integer) request.get(JsonKey.CURRENT_OFFSET);
+    if (currentOffSetFromRequest == null) {
+      currentOffSetFromRequest = 0;
+    }
+    String pageId = (String) request.get(JsonKey.PAGE_ID);
+    String previousPageId = null;
+    int currentOffSet = 1;
+    String currentPagingState = null;
+    do {
+      Response response = cassandraOperation.getRecordByIdentifierWithPage(requestContext, KEYSPACE_NAME,
+          ENROLMENT_BATCH_LOOKUP, queryMap,
+          null, pageId, (Integer) request.get(JsonKey.LIMIT));
+      currentPagingState = (String) response.get(JsonKey.PAGE_ID);
+      if (StringUtils.isBlank(previousPageId) && StringUtils.isNotBlank(currentPagingState)) {
+        previousPageId = currentPagingState;
+      }
+      pageId = currentPagingState;
+      List<Map<String, Object>> userCoursesList = (List<Map<String, Object>>) response.get(JsonKey.RESPONSE);
+      if (CollectionUtils.isEmpty(userCoursesList)) {
+        //Set null so that client knows there are no data to read further.
+        previousPageId = null;
+        break;
+      }
+      for (Map<String, Object> userCourse : userCoursesList) {
+        //From this page, we have already read some records, so skip the records
+        if (currentOffSetFromRequest > 0) {
+          currentOffSetFromRequest--;
+          continue;
+        }
+        if (userCourse.get(JsonKey.ACTIVE) != null
+            && (active == (boolean) userCourse.get(JsonKey.ACTIVE))) {
+          userList.add((String) userCourse.get(JsonKey.USER_ID));
+          if (userList.size() == limit) {
+            //We have read the data... if pageId available send back in response.
+            previousPageId = pageId != null ? pageId : previousPageId;
+            break;
+          }
+        }
+        currentOffSet++;
+      }
+      //We may have read the given limit... if so, break from while loop
+      if (userList.size() == limit) {
+        break;
+      }
+    } while (StringUtils.isNotBlank(currentPagingState));
+    if (StringUtils.isNotBlank(previousPageId)) {
+      result.put(JsonKey.PAGE_ID, previousPageId);
+      if (currentOffSet >= limit) {
+        currentOffSet = currentOffSet - limit;
+      }
+      result.put(JsonKey.CURRENT_OFFSET, currentOffSet);
+    }
+    result.put(JsonKey.COUNT, userList.size());
+    result.put(JsonKey.PARTICIPANTS, userList);
+    return result;
   }
 }
--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1075,6 +1075,6 @@ public final class JsonKey {
   public static final String FIXED_BATCH_ID = "fixedBatchId";
   public static final String COURSE_USER_ENROLMENTS_DB = "course_user_enrolments_db";
   public static final String COURSE_BATCH_PATH = "sunbird_course_batch_path";
-
+  public static final String CURRENT_OFFSET = "currentOffSet";
   private JsonKey() {}
 }
