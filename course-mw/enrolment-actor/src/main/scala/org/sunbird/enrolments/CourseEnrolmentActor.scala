949587903b81c46fa75563dcb7d5483e45c8f5c9
vikrantbipin <118338417+vikrantbipin@users.noreply.github.com>
2023-05-16 15:20:26 +0530

    Cbrelease 4.8.0 (#39)
    * changed logic to update batch lookup table

--- course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/CourseEnrolmentActor.scala
+++ course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/CourseEnrolmentActor.scala
@@ -232,7 +232,7 @@ class CourseEnrolmentActor @Inject()(@Named("course-batch-notification-actor") c
         // code for  find root cause of null value in prod(16-02-2023)
         try {
             val activeStatus = dataMap.get(JsonKey.ACTIVE);
-            logger.info(requestContext, "upsertEnrollment :: IsNew :: " + isNew + " ActiveStatus :: " + activeStatus + " DataMap is :: " + dataMap)
+            logger.info(requestContext, "upsertEnrollment :: IsNew :: " + isNew + " ActiveStatus :: " + activeStatus + " DataMap is :: " + dataMap+ " DataBatchMap:: "+ dataBatchMap)
             if (activeStatus == null) {
                 throw new Exception("Active Value is null in upsertEnrollment");
             }
