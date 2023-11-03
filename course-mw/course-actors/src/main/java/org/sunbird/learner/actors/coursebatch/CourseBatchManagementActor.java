d0e36e2bcef88925549d3df4adf2a781d4fe46ad
dkttarento <138442957+dkttarento@users.noreply.github.com>
2023-11-03 17:50:40 +0530

    added logger

--- course-mw/course-actors/src/main/java/org/sunbird/learner/actors/coursebatch/CourseBatchManagementActor.java
+++ course-mw/course-actors/src/main/java/org/sunbird/learner/actors/coursebatch/CourseBatchManagementActor.java
@@ -582,6 +582,7 @@ public class CourseBatchManagementActor extends BaseActor {
       Date requestedEnrollmentEndDate,
       Date todayDate) {
     Date endDate = requestedEndDate != null ? requestedEndDate : existingEndDate;
+    logger.info(new RequestContext(),"value of enrollment date validation eneabled : "+ enrolmentDateValidationEnabled());
     if (enrolmentDateValidationEnabled() && requestedEnrollmentEndDate != null
         && (requestedEnrollmentEndDate.before(requestedStartDate))) {
       throw new ProjectCommonException(
