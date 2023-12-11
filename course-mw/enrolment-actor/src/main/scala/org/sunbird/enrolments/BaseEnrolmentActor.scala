f88d294390d48896ba98085eae1c6d7343f75773
Karthikeyan Rajendran <70887864+karthik-tarento@users.noreply.github.com>
2023-12-11 16:49:19 +0530

    Fix to eliminate invalid course-batch progress update and to avoid in… (#113)
    * Fix to eliminate invalid course-batch progress update and to avoid invalid kafka producer events for activity aggregator flink job (#112)
    
    * Fix to eliminate invalid course-batch progress update and invalid events
    
    * status updation from 1 to 2
    
    * Fix to handle userID from request in content state update API (#114)
    
    ---------
    
    Co-authored-by: Sreerag K S <58926794+sreeragksgh@users.noreply.github.com>

--- course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/BaseEnrolmentActor.scala
+++ course-mw/enrolment-actor/src/main/scala/org/sunbird/enrolments/BaseEnrolmentActor.scala
@@ -1,20 +1,22 @@
 package org.sunbird.enrolments
 
 import java.util
-
 import org.apache.commons.collections4.CollectionUtils
 import org.sunbird.actor.base.BaseActor
 import org.sunbird.common.ElasticSearchHelper
 import org.sunbird.common.factory.EsClientFactory
 import org.sunbird.common.inf.ElasticSearchService
+import org.sunbird.common.models.response.Response
 import org.sunbird.common.models.util.{JsonKey, ProjectUtil}
 import org.sunbird.common.request.RequestContext
 import org.sunbird.dto.SearchDTO
+import org.sunbird.helper.ServiceFactory
 
 abstract class BaseEnrolmentActor extends BaseActor {
 
     var esService = EsClientFactory.getInstance(JsonKey.REST)
-    
+    private val cassandraOperation = ServiceFactory.getInstance
+
     def getBatches(requestContext: RequestContext, batchIds: java.util.List[String], requestedFields: java.util.List[String]): java.util.List[java.util.Map[String, AnyRef]] = {
         val dto = new SearchDTO
         dto.setLimit(batchIds.size())
@@ -24,6 +26,18 @@ abstract class BaseEnrolmentActor extends BaseActor {
         val future = esService.search(requestContext, dto, ProjectUtil.EsType.courseBatch.getTypeName)
         val response = ElasticSearchHelper.getResponseFromFuture(future).asInstanceOf[java.util.Map[String, AnyRef]]
         response.getOrDefault(JsonKey.CONTENT, new java.util.ArrayList[util.Map[String, AnyRef]]).asInstanceOf[util.List[util.Map[String, AnyRef]]]
+    }
+
+    def getBatchesV2(requestContext: RequestContext, batchId: String, courseId: String, requestedFields: java.util.List[String]): java.util.List[java.util.Map[String, AnyRef]] = {
+        val filters = new java.util.HashMap[String, AnyRef]() {
+            {
+                put(JsonKey.BATCH_ID, batchId)
+                put(JsonKey.COURSE_ID, courseId)
+            }
+        }
+        val response: Response = cassandraOperation.getRecords(requestContext, "sunbird_courses", "course_batch", filters, null)
+        val result: util.Map[String, AnyRef] = response.getResult
+        return result.get(JsonKey.RESPONSE).asInstanceOf[java.util.List[java.util.Map[String, AnyRef]]]
     }
     
     def setEsService(es: ElasticSearchService) = {
