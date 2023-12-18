005846d4c2d7be3d09e0aedd0cd1d67d962684de
saipradeep_ravipati <saipradeep.ravipati@tarento.com>
2023-12-18 13:10:02 +0530

    Added Log for Troubleshooting

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/ContentSearchUtil.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/util/ContentSearchUtil.java
@@ -116,6 +116,7 @@ public class ContentSearchUtil {
 
   public static Map<String, Object> searchContentSync(
           RequestContext requestContext, String urlQueryString, String queryRequestBody, Map<String, String> headers) {
+    logger.error(requestContext,"#TroubleShoot Headers  :- "+headers,null);
     Unirest.clearDefaultHeaders();
     String urlString =
         StringUtils.isNotBlank(urlQueryString)
