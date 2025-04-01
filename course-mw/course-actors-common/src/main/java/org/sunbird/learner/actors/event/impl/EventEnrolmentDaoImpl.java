3f88c9c9b2a6d5215dbe76b837caa3ce866fa891
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-04-01 12:16:13 +0530

    KB-9064 | KB-8013 | Enrolled event for the future date not displaying  under My Event Calendar (#113) (#114)
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
@@ -406,9 +406,12 @@ public class EventEnrolmentDaoImpl implements EventEnrolmentDao {
         if (StringUtils.isNotEmpty(startDateStr) && StringUtils.isNotEmpty(endDateStr)) {
             LocalDate startDate = LocalDate.parse(startDateStr);
             LocalDate endDate = LocalDate.parse(endDateStr);
-            if (startDate.isBefore(LocalDate.parse((String) contentDetails.get("startDate")))
-                    && startDate.isBefore(LocalDate.parse((String) contentDetails.get("endDate")))
-                    && endDate.isAfter(LocalDate.parse((String) contentDetails.get("endDate")))) {
+            LocalDate contentStartDate = LocalDate.parse((String) contentDetails.get(JsonKey.START_DATE));
+            LocalDate contentEndDate = LocalDate.parse((String) contentDetails.get(JsonKey.END_DATE));
+            if ((startDate.isBefore(contentStartDate)
+                    || startDate.isEqual(contentStartDate))
+                    && (endDate.isAfter(contentEndDate)
+                    || endDate.isEqual(contentEndDate))) {
                 addEventDetailsToEnrollment(
                         request,
                         enrollment,
