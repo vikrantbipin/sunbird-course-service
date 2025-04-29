919621ac3ec0e9c10d174c5f61b36911793f8ce7
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-04-29 10:22:35 +0530

    Cbrelease 4.8.23 (#120)
    * commenting for ignore the user auth token. (#119)
    
    * updating the API for illumine
    
    ---------
    
    Co-authored-by: pathiktarento1089 <123535830+pathiktarento1089@users.noreply.github.com>

--- service/app/util/RequestInterceptor.java
+++ service/app/util/RequestInterceptor.java
@@ -51,6 +51,7 @@ public class RequestInterceptor {
     apiHeaderIgnoreMap.put("/v1/blended/program/admin/enroll", var);
     apiHeaderIgnoreMap.put("/v1/event/batch/cert/issue", var);
     apiHeaderIgnoreMap.put("/v2/featured/events", var);
+    apiHeaderIgnoreMap.put("/v1/content/state/admin/update", var); //Exempting this API from authentication as it is used for Illumine integration (no x-auth-token). It is invoked directly by cb-ext service, not via Kong
   }
 
   /**
