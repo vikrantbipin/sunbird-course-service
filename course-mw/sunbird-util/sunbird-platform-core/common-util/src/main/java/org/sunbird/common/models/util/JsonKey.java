62b71101a349e8c21aaced30fb3bb173d99b34d2
karthik-tarento <karthikeyan.rajendran@tarento.com>
2025-10-15 18:56:10 +0530

    Fixed build issue due to merging PR #287

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1270,6 +1270,7 @@ public final class JsonKey {
   public static final String ORGANISATIONS_ROLES = "organisations.roles";
   public static final String EVENT_ENROLLED = "EVENT_ENROLLED";
   public static final String UPDATE_KEY = "UPDATE";
+  public static final String ALERT = "ALERT";
   public static final String RECORDS = "records";
   public static final String CONTENT_READ = "content_read";
   public static final String DATE_KEY = "Date";
