f12660ee14f792bf686cb3097db5dbe765cd59d1
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-10-24 14:39:06 +0530

    Cbrelease 4.8.28 (#298)
    * KB-11813 - LMS Erros : Fixes related to redis issues (#293) (#294)
    
    * LMS Erros - Fixes related to redis issues
    
    * LMS Erros - Fixes related to redis issues
    
    * Review Changes
    
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>
    
    * KB-11852 : LMS Errors - Exception Cassandra query with empty courseId (#297)
    
    * LMS Errors - Exception Cassandra query with empty courseId
    
    * Review Comments
    
    ---------
    
    Co-authored-by: sreerag-tarento <sreerag.sajeesh@tarento.com>

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/coursebatch/dao/impl/UserCoursesDaoImpl.java
@@ -156,7 +156,7 @@ public class UserCoursesDaoImpl implements UserCoursesDao {
 
     List<Map<String, Object>> userCoursesList;
 
-    if (courseIdList != null && courseIdList.size() == 1) {
+    if (CollectionUtils.isNotEmpty(courseIdList) && courseIdList.size() == 1 && StringUtils.isNotBlank(courseIdList.get(0))) {
       primaryKey.put(JsonKey.COURSE_ID_KEY, courseIdList);
       Response response = cassandraOperation.getRecordByIdentifier(requestContext, KEYSPACE_NAME, tableName, primaryKey, null);
       userCoursesList = (List<Map<String, Object>>) response.get(JsonKey.RESPONSE);
