3d16679d33fc8df2260363c3efd7d732427db7d3
jayaprakashnarayanaswamy <jayaprakash.n@tarento.com>
2025-11-17 11:30:27 +0530

    "enabling private listing api for individual event details fetch"

--- service/app/controllers/eventmanagement/EventController.java
+++ service/app/controllers/eventmanagement/EventController.java
@@ -89,7 +89,7 @@ public class EventController extends BaseController {
 
         Map<String, String[]> queryParams = new HashMap<>(httpRequest.queryString());
         if (queryParams.containsKey(JsonKey.EVENT_ID) && queryParams.containsKey(JsonKey.BATCH_ID)) {
-            return getEnrolledEvent(uid, httpRequest);
+            return getEnrolledEvent(uid, httpRequest, true);
         }
         return getEnrolledEventsList(uid, httpRequest, true);
     }
@@ -116,7 +116,7 @@ public class EventController extends BaseController {
                 httpRequest);
     }
 
-    public CompletionStage<Result> getEnrolledEvent(String uid, Http.Request httpRequest) {
+    public CompletionStage<Result> getEnrolledEvent(String uid, Http.Request httpRequest, boolean isPrivate=false) {
         return handleRequest(actorRef, "getEnrol",
                 httpRequest.body().asJson(),
                 (req) -> {
@@ -132,6 +132,9 @@ public class EventController extends BaseController {
                         request.put(JsonKey.BATCH_ID, batchId);
                     }
                     String userId = (String) request.getContext().getOrDefault(JsonKey.REQUESTED_FOR, request.getContext().get(JsonKey.REQUESTED_BY));
+                    if (isPrivate) {
+                        userId = uid;
+                    }
                     validator.validateRequestedBy(userId);
                     if(!userId.equalsIgnoreCase(uid)){
                         ProjectLogger.log("UserId in path and auth token are not same", LoggerEnum.INFO.name());
