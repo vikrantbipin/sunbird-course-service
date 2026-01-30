12170ce505001feeec9ab44a655ead4fe0eeb6db
shankaragoudab-tarento <shankaragouda.biradar@tarento.com>
2026-01-30 18:38:36 +0530

    updated as per review comments

--- service/app/controllers/courseenrollment/validator/CourseEnrollmentRequestValidator.java
+++ service/app/controllers/courseenrollment/validator/CourseEnrollmentRequestValidator.java
@@ -153,6 +153,12 @@ public class CourseEnrollmentRequestValidator extends BaseRequestValidator {
             JsonKey.COURSE_ID);
   }
 
+  /** validateEnrolmentCriteria handles ID extraction/validation
+   * @param requestDto
+   * @param isCourse
+   * @param isBlendedProgram
+   */
+
   public void validateEnrolmentCriteria(Request requestDto, boolean isCourse, boolean isBlendedProgram) {
     // Get the courseId from the request
     String courseId = "";
@@ -176,6 +182,12 @@ public class CourseEnrollmentRequestValidator extends BaseRequestValidator {
     validateAccessSettingsDetails(requestDto, courseId, isCourse, isBlendedProgram);
   }
 
+  /** validateAccessSettingsDetails contains reusable main logic supports other than courseId/programId to validate access settings.
+   * @param requestDto
+   * @param courseId
+   * @param isCourse
+   * @param isBlendedProgram
+   */
   public void validateAccessSettingsDetails(Request requestDto, String courseId, boolean isCourse, boolean isBlendedProgram) {
 
     // Get the course details from ContentCahceHandlerV2
